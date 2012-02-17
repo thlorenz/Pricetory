31e93f27f70cca0e10159867042d478e6018bef5
Thorsten Lorenz <thlorenz@gmx.de>
2012-02-16 23:12:08 -0500

    resolving symbolcode from symbol

--- DataGeneration/RandomDataGenerator.hs
+++ DataGeneration/RandomDataGenerator.hs
@@ -1,6 +1,7 @@
 import qualified Data.ByteString as S;
 import qualified Data.ByteString.Lazy as L;
 import qualified Data.Map as Map;
+import Random (randomR, RandomGen, getStdRandom)
 import Data.Maybe
 import Data.Char (ord)
 import Data.Word
@@ -10,9 +11,9 @@ type SymbolCode   =  Word32
 type Symbol       =  String
 type TimeOffset   =  Word32
 type TimeInterval =  Word32
-type Rate         =  Word32
+type Rate         =  Int
 
-data Tick = Tick TimeOffset Rate
+data Tick = Tick { timeOffset :: TimeOffset, rate :: Rate }
 
 magicNumber :: Word32
 magicNumber = 0x54484f52
@@ -36,9 +37,35 @@ createHeader :: SymbolCode -> TimeOffset -> TimeInterval -> L.ByteString
 createHeader symbol offset interval = 
     (L.concat . map encode) [magicNumber, symbol, offset, interval]
 
-main = do
+writeHeader = do
     let symbol = "EURGBP"
-    let header = (createHeader (getSymbolCode symbol) 0x20 0x0000001)
+    let header = (createHeader (getSymbolCode symbol) 0x20 0x0000001)       
     print $ L.unpack header
+
+nextTick ::  Tick -> Rate -> Tick
+nextTick currentTick rateDelta = Tick nextTimeOffset nextRate 
+    where 
+        nextTimeOffset = (+1) $ timeOffset currentTick
+        nextRate = (+rateDelta) $ rate currentTick
+
+randomRateDelta :: (RandomGen g) => g -> (Rate, g)
+randomRateDelta gen = (deltas !! index, nextGen)
+    where
+        (index, nextGen)  = randomR (0, upperBound) gen 
+        upperBound = (length deltas) - 1 
+
+        -- smaller rate changes should occur more often
+        deltas :: [Rate]
+        deltas = 
+            (replicate 5 (-1)) ++ (replicate 5 1) ++
+            (replicate 3 (-2)) ++ (replicate 3 2) ++
+            (replicate 2 (-3)) ++ (replicate 2 3) ++
+            [4, (-4), 5, (-5)]
+
+main = do
+    (getStdRandom $ randomRateDelta) >>= print
+
+
+
 
 
