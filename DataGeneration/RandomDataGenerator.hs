d3918bef30c93b0926a38d195619283833cf7afd
Thorsten Lorenz <thlorenz@gmx.de>
2012-02-16 22:13:47 -0500

    resolving symbolcode from symbol

--- DataGeneration/RandomDataGenerator.hs
+++ DataGeneration/RandomDataGenerator.hs
@@ -1,5 +1,7 @@
 import qualified Data.ByteString as S;
 import qualified Data.ByteString.Lazy as L;
+import qualified Data.Map as Map;
+import Data.Maybe
 import Data.Char (ord)
 import Data.Word
 import Data.Binary
@@ -15,21 +17,28 @@ data Tick = Tick TimeOffset Rate
 magicNumber :: Word32
 magicNumber = 0x54484f52
 
-symbolMap :: [(Symbol, SymbolCode)]
-symbolMap = [ ("EURUSD", 0x00000001)
-            , ("EURGBP", 0x00000002)
-            , ("USDCAD", 0x00000003)
-            , ("USDCHF", 0x00000004)
-            , ("USDCNY", 0x00000005)
-            ]
+symbolMap ::  Map.Map Symbol SymbolCode
+symbolMap = Map.fromList $
+    [ ("EURUSD", 0x00000001)
+    , ("EURGBP", 0x00000002)
+    , ("USDCAD", 0x00000003)
+    , ("USDCHF", 0x00000004)
+    , ("USDCNY", 0x00000005)
+    ] 
 
--- TODO add symbolcode mapping function go symbol
+getSymbolCode :: Symbol -> SymbolCode
+getSymbolCode symbol = 
+    case Map.lookup symbol symbolMap of
+        Just code       -> code 
+        Nothing         -> head $ (Map.elems symbolMap)
 
 createHeader :: SymbolCode -> TimeOffset -> TimeInterval -> L.ByteString
 createHeader symbol offset interval = 
     (L.concat . map encode) [magicNumber, symbol, offset, interval]
 
 main = do
-    let header = (createHeader 0x00000001 0x20 0x0000001)
+    let symbol = "EURGBP"
+    let header = (createHeader (getSymbolCode symbol) 0x20 0x0000001)
     print $ L.unpack header
+
 
