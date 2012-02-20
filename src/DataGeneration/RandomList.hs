40f90257c18cd9eb21948495377ff6620751a217
Thorsten Lorenz <thlorenz@gmx.de>
2012-02-20 16:36:06 -0500

    cleaning up

--- src/DataGeneration/RandomList.hs
+++
@@ -1,7 +0,0 @@
-import Random (mkStdGen, randomRs)
-
-getRandomIndexes gen max = randomRs (min, max) gen
-    where min = (0 :: Int) 
-
-main = do
-    print $ take 20 $ getRandomIndexes (mkStdGen 200) 5
