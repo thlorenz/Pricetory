5386fa33fd6000c0adf3cd9eeb41383d91e21a9f
Thorsten Lorenz <thlorenz@gmx.de>
2012-02-22 22:15:48 -0500

    implemented sample bytestrings function - bytes need to still substract the skipped bytes as well during recursion

--- src/Data/Sampler.hs
+++ src/Data/Sampler.hs
@@ -19,16 +19,36 @@ dataDir = "/Users/thlorenz/dev/data/Pricetory"
 symbolName = "EURUSD"
 fileName = dataDir ++ "/" ++ symbolName ++ ".bin"
 
+word = 4
+tickSize = 2 * word
+
 -- | TODO: handle Nothing which signifies an exception
 readHeader ::  Handle -> IO Header
-readHeader fh = liftM (fromJust . decodeFileHeader) $ L.hGet fh (5 * 4) 
+readHeader fh = liftM (fromJust . decodeFileHeader) $ L.hGet fh bytesInHeader
+    where bytesInHeader = 5 * word
+
+{- | Samples byte strings from a given file handle
+     h          - the file handle
+     bytes      - the number of bytes after which to stop reading (usually the end of the file)
+     skipNum    - the number of ticks to skip in between each read
+-}
+sampleByteStrings :: Handle -> Int -> Int -> IO [L.ByteString]
+sampleByteStrings h bytes skipNum = sample bytes []
+    where sample bytes xs = do 
+            x <- L.hGet h tickSize
+            if bytes > 0 then do 
+                 hSeek h RelativeSeek $ fromIntegral (tickSize * skipNum)
+                 sample (bytes - tickSize) (x:xs)
+            else return $ reverse xs
 
 main = do
-    fh <- openFile fileName ReadMode
-    print =<< readHeader fh
+    h <- openBinaryFile fileName ReadMode
+    print =<< readHeader h
 
-    -- hSeek hp AbsoluteSeek x
-    hClose fh
+    print =<< sampleByteStrings h 200 60
+    hClose h
     
     putStrLn "\n--------------\nDone!"
 
+-- Reader monad via data-ivar, write-once variables
+-- http://hackage.haskell.org/packages/archive/data-ivar/0.30/doc/html/Data-IVar.html
