aba646f7b066437da7eb72795c5019fadac6399f
Thorsten Lorenz <thlorenz@gmx.de>
2012-02-26 22:50:55 -0500

    supplying server with world of tickdata (only eurusd at this point)

--- src/Network/TCPServer.hs
+++ src/Network/TCPServer.hs
@@ -4,25 +4,37 @@ module Network.TCPServer where
 
 import System.Console.CmdArgs
 import Network (listenOn, accept, PortID(..), Socket)
-import System.IO (hSetBuffering, hGetLine, hPutStrLn, BufferMode(..), Handle)
+import System.IO (hSetBuffering, hGetLine, hPutStrLn, BufferMode(..), Handle, FilePath)
 import Control.Concurrent (forkIO)
 
-data Arguments = Arguments { host :: String, port :: Int } deriving (Show, Data, Typeable)
+import Data.Sampler (getWorldOfTickData)
+import Contract.Types
+import Contract.Symbols
+
+data Arguments = Arguments { host       :: String
+                           , port       :: Int
+                           , dataFolder :: FilePath
+                           } deriving (Show, Data, Typeable)
 
 arguments = Arguments
     { host = "localhost" &= typ "String" &= help "Host server is running on"
     , port = 3000        &= typ "Int"    &= help "Port server is listening on for incoming connections"
+    , dataFolder = defFolder &= typ "FilePath" &= help "The folder in which the data resides"
     } &= summary "Pricetory TCP Server version 0.0.1"
+    where defFolder = "/Users/thlorenz/dev/data/Pricetory"
 
 main :: IO ()
 main = do
     args <- cmdArgs arguments
     sock <- (listenOn . PortNumber . fromIntegral . port) args
-    putStrLn $ "Listening on " ++ (host args) ++ ":" ++ (show $ port args) ++ "."
-    sockHandler sock
+    putStrLn $ "Getting world of tickdata from " ++ (dataFolder args)
+    worldOfTickData <- getWorldOfTickData (dataFolder args) [eurusd] 
 
-sockHandler :: Socket -> IO ()
-sockHandler sock = do
+    putStrLn $ "Listening on " ++ (host args) ++ ":" ++ (show $ port args) ++ "."
+    sockHandler sock worldOfTickData 
+
+sockHandler :: Socket -> HistoricalTickDataMap -> IO ()
+sockHandler sock world = do
     (handle, host, port) <- accept sock
     putStrLn $ "Accepted " ++ (show host) ++ ":" ++ (show port)
     
@@ -32,7 +44,7 @@ sockHandler sock = do
     forkIO $ commandProcessor handle
 
     -- handle more incoming connections
-    sockHandler sock
+    sockHandler sock world
 
 commandProcessor :: Handle -> IO () 
 commandProcessor handle = do
