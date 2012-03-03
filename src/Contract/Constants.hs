0b668eda60b91eefa9ebba5ddb17c81fd3fb546c
Thorsten Lorenz <thlorenz@gmx.de>
2012-03-03 12:16:21 -0500

    generating random request:

--- src/Contract/Constants.hs
+++ src/Contract/Constants.hs
@@ -2,8 +2,11 @@ module Contract.Constants where
 
 import Contract.Types
 
-secondsPerMinute = 60 :: TimeInterval
-minutesPerHour   = 60 :: TimeInterval
-hoursPerDay      = 24 :: TimeInterval  
+secondsPerMinute = 60  :: TimeInterval
+minutesPerHour   = 60  :: TimeInterval
+hoursPerDay      = 24  :: TimeInterval
+daysPerYear      = 365 :: TimeInterval
 secondsPerHour   = secondsPerMinute * minutesPerHour
 secondsPerDay    = secondsPerHour * hoursPerDay
+secondsPerYear   = secondsPerDay * daysPerYear 
+
---
+++ src/Network/RandomRequestGenerator.hs
@@ -0,0 +1,64 @@
+module Network.RandomRequestGenerator where
+
+import Random (randomR, RandomGen, StdGen, Random, mkStdGen, getStdGen)
+import Data.Word
+import Contract.Types
+import Contract.Constants
+
+instruments = 3 :: SymbolCode
+maxSeconds = secondsPerDay
+oneSecProb = 5
+oneMinProb = 40
+oneHourProb = 35
+
+entireDataSecs = secondsPerDay * daysPerYear :: TimeOffset
+
+data Bound = Bound 
+    { bndInterval :: TimeInterval
+    , bndTimeSpan :: TimeInterval
+    }
+
+randSymbolCode :: StdGen -> (SymbolCode, StdGen)
+randSymbolCode g = (fromIntegral . fst $ res, snd res)
+    where res = randomR (1, fromIntegral instruments) g :: (Int, StdGen)
+
+randTimeInterval :: (TimeInterval, TimeInterval) -> StdGen -> (TimeInterval, StdGen)
+randTimeInterval (min, max) g = (fromIntegral . fst $ res, snd res)
+    where res = randomR (fromIntegral min, fromIntegral max) g :: (Int, StdGen)
+
+randStartOffset :: TimeOffset -> TimeInterval -> StdGen -> (TimeOffset, StdGen)
+randStartOffset eofOffset len g = (fromIntegral . fst $ res, snd res)
+    where max = eofOffset - len
+          res = randomR (fromIntegral min, fromIntegral max) g :: (Int, StdGen)
+          min = 1
+
+generateRequest :: StdGen -> (Request, StdGen)
+generateRequest g = (req, g3)
+    where 
+        req = Request symCode 
+                      startOffset 
+                      (startOffset + bndTimeSpan bound) 
+                      (bndInterval bound)
+
+        (symCode, g1) = randSymbolCode g :: (SymbolCode, StdGen)
+        (bound, g2)  = let (index, bg) = randomR (0, length bounds - 1) g1
+                       in (bounds !! index, bg)
+        (startOffset, g3) = randStartOffset entireDataSecs (bndTimeSpan bound) g2
+
+{-  Bounds are setup to reflect below distribution 
+
+    Interval:       1 sec       1 min       1 hour
+    Probability:    5%          40%         35%
+    Max Timespan:   15 mins     600 mins    1 year
+    Max Read Ticks: 900         600         8750
+    Faults to Disk: N           N           Y -}
+
+bounds =   (replicate oneSecProb secondsBnds) ++
+           (replicate oneMinProb minutesBnds) ++
+           (replicate oneHourProb hoursBnds)
+    where
+        secondsBnds = Bound (1 :: Word32) (15 * secondsPerMinute)
+        minutesBnds = Bound secondsPerMinute (600 * secondsPerMinute)
+        hoursBnds   = Bound (secondsPerDay * daysPerYear) (secondsPerDay * daysPerYear)
+
+main = getStdGen >>= print . generateRequest
